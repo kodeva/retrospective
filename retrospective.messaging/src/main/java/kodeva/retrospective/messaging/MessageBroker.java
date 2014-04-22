63cd6ec8cb12f046bcb823d243d90b74595c1eff
Stepan Hrbacek <stepan.hrbacek@gmail.com>
2014-04-22 22:28:34 +0200

    JavaDoc

--- retrospective.messaging/src/main/java/kodeva/retrospective/messaging/MessageBroker.java
+++ retrospective.messaging/src/main/java/kodeva/retrospective/messaging/MessageBroker.java
@@ -6,10 +6,21 @@ package kodeva.retrospective.messaging;
  * @author Stepan Hrbacek
  */
 public class MessageBroker {
+	/**
+	 * Send a message to all matching subscribers.
+	 * 
+	 * @param message
+	 */
 	public void sendMessage(Message message) {
 		
 	}
 	
+	/**
+	 * Adds the processor (subscriber) for messages matching the filter.
+	 *  
+	 * @param filter
+	 * @param processor
+	 */
 	public void subscribe(MessageFilter filter, MessageProcessor processor) {
 		
 	}
